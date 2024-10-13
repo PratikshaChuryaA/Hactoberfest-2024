38dc60342be0efddccd5a434235d6cd089e4a301
pjha8040 <pjha8040@gmail.com>
2024-10-14 00:42:07 +0530

    Added LC hard problem Regular Exprression Matching

---
+++ Regular_expression_matching.py
@@ -0,0 +1,18 @@
+def isMatch(s: str, p: str) -> bool:
+    def dfs(i, j):
+        if i >= len(s) and j >= len(p):
+            return True
+        if j >= len(p):
+            return False
+        
+        match = i < len(s) and (s[i] == p[j] or p[j] == ".")
+        
+        if j + 1 < len(p) and p[j + 1] == "*":
+            return dfs(i, j + 2) or (match and dfs(i + 1, j))
+        
+        if match:
+            return dfs(i + 1, j + 1)
+        
+        return False
+    
+    return dfs(0, 0)
